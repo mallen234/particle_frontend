74177386cf8b75b4acada3c71226682768be3572
Marcus Allen <marcusallen23@hotmail.co.uk>
2023-06-06 21:37:41 +0100

    added logic to now use containerised code with SS sim

--- src/controls.js
+++ src/controls.js
@@ -1,14 +1,15 @@
 // controls.js
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import '../style.css'
+import { TextureLoader } from "three";
 
 
 export const createControls = (camera, canvas) => {
   const controls = new OrbitControls(camera, canvas);
   controls.enableDamping = true;
   controls.enablePan = false;
-  controls.enableZoom = false;
+  controls.enableZoom = true;
   controls.autoRotate = true;
-  controls.autoRotateSpeed = 5;
+  controls.autoRotateSpeed = 0;
   return controls;
 };
--- src/scene.js
+++ src/scene.js
@@ -6,16 +6,23 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 export const createScene = () => {
   const scene = new THREE.Scene();
 
-  const geometry = new THREE.SphereGeometry(3, 64, 64);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffa500 });
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  const geometry = new THREE.SphereGeometry(0.1, 64, 64);
 
-  const light = new THREE.PointLight(0xffffff, 1, 100);
+  const Mmaterial = new THREE.MeshStandardMaterial({ color: 0x9ec2535 });
+  const Ematerial = new THREE.MeshStandardMaterial({ color: 0x00FFFF });
+  const Smaterial = new THREE.MeshStandardMaterial({ color: 0xffa500 });
+  const sun = new THREE.Mesh(geometry, Smaterial);
+  const earth = new THREE.Mesh(geometry, Ematerial);
+  const mars = new THREE.Mesh(geometry, Mmaterial);
+
+  scene.add(sun,earth,mars);
+
+  const light = new THREE.PointLight(0xffffff, 1, 100); 
   const light_2 = new THREE.AmbientLight(0xffffff, 1, 100);
   light.position.set(0, 10, 10);
-  scene.add(light);
+  light_2.position.set(0, 10, 10);
+  scene.add(light,light_2);
 
-  return { scene, mesh, light };
+  return { scene, sun, earth, mars, light };
 };
 
